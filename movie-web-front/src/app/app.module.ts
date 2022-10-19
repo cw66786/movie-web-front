20bdd029e71f6bb9130a30e53909168230bc0d4f
Craig Wilson <cw66786@gmail.com>
2022-10-19 18:52:29 -0500

    created home component

--- movie-web-front/src/app/app.module.ts
+++ movie-web-front/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
