288b39727a7c815535a9013239b5e0cef261a956
Craig Wilson <cw66786@gmail.com>
2022-10-20 00:44:49 -0500

    added mobil responsivness to home page and header

--- movie-web-front/src/app/home/hero/hero.component.ts
+++ movie-web-front/src/app/home/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterModule, Routes, Router } from '@angular/router';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  
+
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.router.navigateByUrl('/signUp');
+}
 }
