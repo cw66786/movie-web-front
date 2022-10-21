2e60e42147d02cacaccbf8e3b5ffdabcb24860f8
Craig Wilson <cw66786@gmail.com>
2022-10-20 21:40:57 -0500

    created register component

--- movie-web-front/src/app/app.module.ts
+++ movie-web-front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HeroComponent } from './home/hero/hero.component';
 import { SignInComponent } from './signIn/signIn.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SignInComponent } from './signIn/signIn.component';
     FooterComponent,
     HomeComponent,
     HeroComponent,
-    SignInComponent
+    SignInComponent,
+    RegisterComponent
     
   ],
   imports: [
