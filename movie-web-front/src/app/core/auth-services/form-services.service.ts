5061ec1669dd7c05159f14817e98ca151e2d20fd
Craig Wilson <cw66786@gmail.com>
2022-11-11 23:22:52 -0600

    added davids back end added async email validator

--- movie-web-front/src/app/core/auth-services/form-services.service.ts
+++ movie-web-front/src/app/core/auth-services/form-services.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { debounceTime, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormServicesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  checkEmail(userEmail: string){
+    const url = 'http://localhost:4231/auth/check-email';
+
+    return this.http.post(url,{email: userEmail});
+
+  }
+
+
 }
--- movie-web-front/src/app/pages/register/form-one/form-one.component.ts
+++ movie-web-front/src/app/pages/register/form-one/form-one.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators, AsyncValidatorFn, AsyncValidator } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { debounceTime, map, Observable, of } from 'rxjs';
+import { FormServicesService } from 'src/app/core/auth-services/form-services.service';
 
 import { FormErrorService } from 'src/app/core/form-error.service';
 
@@ -17,11 +18,11 @@ export class FormOneComponent implements OnInit {
 
   
 
-  constructor(public errorMatcher: FormErrorService, private fb: FormBuilder) {}
+  constructor(public errorMatcher: FormErrorService, private fb: FormBuilder,private formService: FormServicesService) {}
 
   ngOnInit(): void {
     this.registerForm1 = this.fb.group({
-      email: new FormControl('', [Validators.email, Validators.required ]),
+      email: new FormControl('', [Validators.email, Validators.required ],[this.emailChecker]),
   
       confirmEmail: new FormControl('', []),
     },{validators: this.checkConfirm});    
@@ -35,8 +36,21 @@ export class FormOneComponent implements OnInit {
   }
   
   
- emailChecker: AsyncValidatorFn = (control: FormControl): Observable<ValidationErrors> | null => {
-        return of({taken: 'true'});
+ emailChecker: AsyncValidatorFn = (control: FormControl): Observable<ValidationErrors | null> => {
+    
+  return this.formService.checkEmail(control.value).pipe(
+    debounceTime(500),
+    map((data: any)=>{
+      
+      if(data === true){
+        
+        return {taken: true};
+      };
+      return null;
+
+    })
+  )
+   
  }
 
 
